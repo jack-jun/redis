65fb959b4cd78c53440733aa59885fdacc176575
jack-jun <liuluoliu2008@gmail.com>
2016-08-30 17:03:24 +0800

    data files

--- redis.js
+++ redis.js
@@ -1,6 +1,6 @@
 var redis = require('redis'),
     RDS_PORT = 6379,        //端口号
-    RDS_HOST = '127.0.1.1',    //服务器IP
+    RDS_HOST = '127.0.0.1',    //服务器IP
     RDS_PWD = 'porschev',    //密码    
     RDS_OPTS = {},            //设置项
     client = redis.createClient(RDS_PORT,RDS_HOST,RDS_OPTS);
